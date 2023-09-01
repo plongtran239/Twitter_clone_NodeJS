04ba3b57aeb47f803473457e43e17c3d9de3d124
plongtran239 <plongtran239@gmail.com>
2023-09-01 16:52:19 +0700

    Add Socket Middleware

--- src/index.ts
+++ src/index.ts
@@ -113,6 +113,7 @@ io.use(async (socket, next) => {
     }
     // Truyền decoded_authorization vào socket để sử dụng ở các middleware khác
     socket.handshake.auth.decoded_authorization = decoded_authorization
+    socket.handshake.auth.access_token = access_token
     next()
   } catch (error) {
     next({
@@ -129,6 +130,22 @@ io.on('connection', (socket) => {
   users[user_id] = {
     socket_id: socket.id
   }
+
+  socket.use(async (packet, next) => {
+    const { access_token } = socket.handshake.auth
+    try {
+      await verifyAccessToken(access_token)
+      next()
+    } catch (error) {
+      next(new Error('Unauthorized'))
+    }
+  })
+
+  socket.on('error', (error) => {
+    if (error.message === 'Unauthorized') {
+      socket.disconnect()
+    }
+  })
 
   socket.on('send_message', async (data) => {
     const { receiver_id, sender_id, content } = data.payload
