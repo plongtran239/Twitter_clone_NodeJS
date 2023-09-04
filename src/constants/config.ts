48dc50031622798f943cd59305ab2659b3a84579
plongtran239 <plongtran239@gmail.com>
2023-09-04 13:20:40 +0700

    Use NODE_ENV and PM2

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,13 +1,29 @@
 import { config } from 'dotenv'
-import argv from 'minimist'
+import fs from 'fs'
+import path from 'path'
 
-const options = argv(process.argv.slice(2))
+const env = process.env.NODE_ENV
 
-export const isProduction = options.env === 'production'
+const envFilename = `.env.${env}`
+
+if (!env) {
+  console.log(`Bạn chưa cung cấp biến môi trường NODE_ENV (ví dụ: development, production)`)
+  console.log(`Phát hiện NODE_ENV = ${env}`)
+  process.exit(1)
+}
+console.log(`Phát hiện NODE_ENV = ${env}, vì thế app sẽ dùng file môi trường là ${envFilename}`)
+if (!fs.existsSync(path.resolve(envFilename))) {
+  console.log(`Không tìm thấy file môi trường ${envFilename}`)
+  console.log(`Lưu ý: App không dùng file .env, ví dụ môi trường là development thì app sẽ dùng file .env.development`)
+  console.log(`Vui lòng tạo file ${envFilename} và tham khảo nội dung ở file .env.example`)
+  process.exit(1)
+}
 
 config({
-  path: options.env ? `.env.${options.env}` : '.env'
+  path: envFilename
 })
+
+export const isProduction = env === 'production'
 
 export const envConfig = {
   port: process.env.PORT as string,
